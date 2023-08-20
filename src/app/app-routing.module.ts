a410e695bf09408e1caf023c2d8e2fd81214e1d3
thuansystemit <thuan.system@gmail.com>
2023-08-20 12:20:01 +0700

    Update code is handing reload page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,7 +17,11 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  imports: [RouterModule.forRoot(routes, {
+    onSameUrlNavigation: 'reload',
+    anchorScrolling: 'enabled',
+    enableTracing: false
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
